f30481dbce1cf1184526a9fea26a5be6603e03a2
VirtualBrightPlayz <lego.trains.b@gmail.com>
2021-08-11 18:09:16 -0700

    update assimp and fbx exporter

--- CBRE.Editor/Compiling/GenericExport.cs
+++ CBRE.Editor/Compiling/GenericExport.cs
@@ -4,7 +4,7 @@ using CBRE.Providers.Model;
 namespace CBRE.Editor.Compiling {
     class GenericExport {
         public static void SaveToFile(string filename, Document document, string format) {
-            AssimpProvider.SaveToFile(filename, document.Map, format);
+            AssimpProvider.SaveToFile(filename, document.Map, document.GameData, format);
         }
     }
 }
