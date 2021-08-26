5a4724a36b8572cd1570037aa57c2de143d34756
VirtualBrightPlayz <lego.trains.b@gmail.com>
2021-08-25 19:22:56 -0700

    working lua brushes

--- CBRE.Editor/Scripting/LuaAPI/LuaBrush.cs
+++ CBRE.Editor/Scripting/LuaAPI/LuaBrush.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using CBRE.Common;
@@ -14,30 +15,37 @@ namespace CBRE.Editor.Scripting.LuaAPI {
         public LuaBrush(Script lua)
         {
             this.lua = lua;
+            lua.Globals["Vector3"] = DynValue.NewCallback(NewVector3);
+        }
+
+        private DynValue NewVector3(ScriptExecutionContext arg1, CallbackArguments arg2) {
+            DynValue tbl = DynValue.NewTable(lua);
+            tbl.Table.Set(1, arg2[1]);
+            tbl.Table.Set(2, arg2[2]);
+            tbl.Table.Set(3, arg2[3]);
+            return tbl;
         }
 
         public string Name => (string)lua.Globals["Name"];
-
-        public int FaceCount => (int)lua.Globals["FaceCount"];
-        public int VertCount => (int)lua.Globals["VertCount"];
 
         public bool CanRound => throw new System.NotImplementedException();
 
         public IEnumerable<MapObject> Create(IDGenerator generator, Box box, ITexture texture, int roundDecimals) {
             var solid = new Solid(generator.GetNextObjectID()) { Colour = Colour.GetRandomBrushColour() };
             DynValue val = lua.Call(lua.Globals["Create"], box);
-            float[][][] faces = val.ToObject<float[][][]>();
-            Vector3 toV3(float[] x) {
-                return new Vector3((decimal)x[0], (decimal)x[1], (decimal)x[2]);
+            Table faces = val.Table;
+            Vector3 toV3(Table x) {
+                return new Vector3((decimal)x.Get(1).Number, (decimal)x.Get(2).Number, (decimal)x.Get(3).Number);
             }
-            foreach (var arr in faces) {
+            foreach (var arr in faces.Values) {
+                var titem = arr.Table;
                 var face = new Face(generator.GetNextFaceID()) {
                     Parent = solid,
-                    Plane = new Plane(toV3(arr[0]), toV3(arr[1]), toV3(arr[2])),
+                    Plane = new Plane(toV3(titem.Get(1).Table), toV3(titem.Get(2).Table), toV3(titem.Get(3).Table)),
                     Colour = solid.Colour,
                     Texture = { Texture = texture }
                 };
-                face.Vertices.AddRange(arr.Select(x => new Vertex(toV3(x).Round(roundDecimals), face)));
+                face.Vertices.AddRange(titem.Values.Select(x => new Vertex(toV3(x.Table).Round(roundDecimals), face)));
                 face.UpdateBoundingBox();
                 face.AlignTextureToFace();
                 solid.Faces.Add(face);
