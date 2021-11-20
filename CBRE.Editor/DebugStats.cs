885f3c9442f311c4d12ac9877b38715e361885c8
Juan Pablo Arce <juanjprot@gmail.com>
2021-11-20 12:26:57 -0300

    Added x86 configuration (TODO: fix native dependency copying)

--- CBRE.Editor/DebugStats.cs
+++ CBRE.Editor/DebugStats.cs
@@ -17,19 +17,19 @@ namespace CBRE.Editor {
             string bytesToMiB(long byteCount)
                 => $"{(byteCount / 1024.0f / 1024.0f): 0.0} MiB";
 
-            yield return ($"Working set: {bytesToMiB(proc.WorkingSet64)}");
-            yield return ($"Private mem: {bytesToMiB(proc.PrivateMemorySize64)}");
-            yield return ($"Paged mem: {bytesToMiB(proc.PagedMemorySize64)}");
-            yield return ($"GC.GetTotalMemory: {bytesToMiB(GC.GetTotalMemory(false))}");
-            yield return ($"GC.GetTotalAllocatedBytes: {bytesToMiB(GC.GetTotalAllocatedBytes(false))}");
-            yield return ($"Gen 0 collections: {GC.CollectionCount(0)}");
-            yield return ($"Gen 1 collections: {GC.CollectionCount(1)}");
-            yield return ($"Gen 2 collections: {GC.CollectionCount(2)}");
-            yield return ($"Textures loaded: {TextureProvider.Packages.Sum(p => p.Items.Count())}");
-            yield return ($"Texture buffer size total: "
+            yield return $"Working set: {bytesToMiB(proc.WorkingSet64)}";
+            yield return $"Private mem: {bytesToMiB(proc.PrivateMemorySize64)}";
+            yield return $"Paged mem: {bytesToMiB(proc.PagedMemorySize64)}";
+            yield return $"GC.GetTotalMemory: {bytesToMiB(GC.GetTotalMemory(false))}";
+            yield return $"GC.GetTotalAllocatedBytes: {bytesToMiB(GC.GetTotalAllocatedBytes(false))}";
+            yield return $"Gen 0 collections: {GC.CollectionCount(0)}";
+            yield return $"Gen 1 collections: {GC.CollectionCount(1)}";
+            yield return $"Gen 2 collections: {GC.CollectionCount(2)}";
+            yield return $"Textures loaded: {TextureProvider.Packages.Sum(p => p.Items.Count())}";
+            yield return $"Texture buffer size total: "
                 + bytesToMiB(TextureProvider.Packages.Sum(
                 p => p.Items.Sum(
-                    i => i.Value.Texture is AsyncTexture { DataBufferSize: int bufferSz } ? bufferSz : 0))));
+                    i => i.Value.Texture is AsyncTexture { DataBufferSize: int bufferSz } ? bufferSz : 0)));
         }
     }
 }
