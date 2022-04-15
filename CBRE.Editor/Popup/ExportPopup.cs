b31c94399c26b3935e45ba5b1f704721f519b0a9
juanjp600 <juanjprot@gmail.com>
2022-04-15 18:56:04 -0300

    Reimplemented lightmap settings window

--- CBRE.Editor/Popup/ExportPopup.cs
+++ CBRE.Editor/Popup/ExportPopup.cs
@@ -1,36 +1,84 @@
 using System;
-using System.Threading.Tasks;
-using CBRE.Editor.Compiling;
 using CBRE.Editor.Compiling.Lightmap;
 using CBRE.Editor.Documents;
-using CBRE.Providers.Model;
 using CBRE.Settings;
 using ImGuiNET;
-using NativeFileDialog;
+using Microsoft.Xna.Framework;
+using Num = System.Numerics;
 
 namespace CBRE.Editor.Popup {
     public class ExportPopup : PopupUI {
-        public enum LightmapSize : int {
-            Tiny = 128,
-            Small = 256,
-            Normal = 512,
-            Big = 1024,
-            // VRamEater = 2048,
-        }
+        protected override bool canBeDefocused => false;
+        protected override bool hasOkButton => false;
 
-        private Document document;
-        private LightmapSize size = LightmapSize.Normal;
+        private readonly Document document;
+        private int textureDims;
         private float downscaleFactor;
+        private int blurRadius;
+        private Color ambientLightColor;
+
+        private readonly Lightmapper lightmapper;
 
         public ExportPopup(Document document) : base("Export / Compile") {
             this.document = document;
+            textureDims = LightmapConfig.TextureDims;
             downscaleFactor = LightmapConfig.DownscaleFactor;
-            var lightmapper = new Lightmapper(document);
-            lightmapper.Render();
+            ambientLightColor = new Color(
+                (byte)LightmapConfig.AmbientColorR,
+                (byte)LightmapConfig.AmbientColorG,
+                (byte)LightmapConfig.AmbientColorB);
+            blurRadius = LightmapConfig.BlurRadius;
+            lightmapper = new Lightmapper(document);
         }
 
         protected override void ImGuiLayout(out bool shouldBeOpen) {
-            shouldBeOpen = false;
+            ImGui.SetWindowSize(new Num.Vector2(300,200), ImGuiCond.FirstUseEver);
+            shouldBeOpen = true;
+
+            ImGui.PushItemWidth(100.0f);
+            
+            ImGui.Text("Lightmap dimensions");
+            ImGui.SameLine();
+            ImGui.InputInt("##lmDim", ref textureDims);
+            
+            float quality = 100.0f / downscaleFactor;
+            ImGui.Text("Quality");
+            ImGui.SameLine();
+            ImGui.InputFloat("##quality", ref quality);
+            quality = MathF.Max(quality, 0.01f);
+            downscaleFactor = MathF.Round(100.0f / quality);
+            ImGui.SameLine();
+            ImGui.Text("    Downscale factor");
+            ImGui.SameLine();
+            ImGui.InputFloat("##downscaleFactor", ref downscaleFactor);
+            downscaleFactor = MathF.Max(1.0f, MathF.Abs(downscaleFactor));
+            
+            ImGui.Text("Blur radius");
+            ImGui.SameLine();
+            ImGui.InputInt("##blurRadius", ref blurRadius);
+            
+            ImGui.PopItemWidth();
+
+            ImGui.PushItemWidth(200.0f);
+            
+            ImGui.Text("Ambient light color");
+            Num.Vector3 ambientLight = new(ambientLightColor.R / 255.0f, ambientLightColor.G / 255.0f, ambientLightColor.B / 255.0f);
+            ImGui.ColorPicker3("##ambientLight", ref ambientLight);
+            ambientLightColor = new Color(ambientLight.X, ambientLight.Y, ambientLight.Z);
+            
+            ImGui.PopItemWidth();
+            
+            ImGui.PushItemWidth(200.0f);
+            if (ImGui.Button("Render")) {
+                LightmapConfig.DownscaleFactor = downscaleFactor;
+                LightmapConfig.BlurRadius = blurRadius;
+                LightmapConfig.TextureDims = textureDims;
+                LightmapConfig.AmbientColorR = ambientLightColor.R;
+                LightmapConfig.AmbientColorG = ambientLightColor.G;
+                LightmapConfig.AmbientColorB = ambientLightColor.B;
+                lightmapper.Render();
+            }
+            ImGui.PopItemWidth();
         }
     }
 }
