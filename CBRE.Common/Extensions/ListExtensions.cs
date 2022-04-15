cf3770e1cd8c9ccd164e9b1c1a239ba78aac747e
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-04-15 21:00:48 +0200

    Add maximum and trigger on saving & opening

---
+++ CBRE.Common/Extensions/ListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CBRE.Common.Extensions;
+   public static class ListExtensions {
+       public static void Remove<T>(this List<T> list, Range range) {
+           if (!range.Start.IsFromEnd && range.Start.Value > list.Count) {
+               return;
+           }
+           var (offset, length) = range.GetOffsetAndLength(list.Count);
+           list.RemoveRange(offset, length);
+       }
+   }
