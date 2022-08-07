a0b853dd52d0b1432f22b500fde13f2f7e679dfb
VirtualBrightPlayz <lego.trains.b@gmail.com>
2022-08-06 18:19:31 -0700

    fixes

--- CBRE.Editor/Tools/Widgets/ImGuizmoWidget.cs
+++ CBRE.Editor/Tools/Widgets/ImGuizmoWidget.cs
@@ -3,9 +3,11 @@ using CBRE.Editor.Documents;
 using CBRE.Editor.Rendering;
 using CBRE.Editor.Tools.Widgets;
 using CBRE.Graphics;
+using CBRE.Settings;
 using ImGuiNET;
 using ImGuizmoNET;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace CBRE.Editor.Tools.Widgets {
     public class ImGuizmoWidget : Widget {
@@ -33,31 +35,30 @@ namespace CBRE.Editor.Tools.Widgets {
 
         public override void ViewportUi(ViewportBase viewport) {
             if (viewport is Viewport3D vp3d) {
+                var keyboardState = Keyboard.GetState();
                 float[] view = vp3d.GetCameraMatrix().ToCbre().Values.Select(x => (float)x).ToArray();
                 float[] proj = vp3d.GetViewportMatrix().ToCbre().Values.Select(x => (float)x).ToArray();
-                matWorld = Matrix.CreateTranslation(Document.Selection.GetSelectionBoundingBox()?.Center.ToXna() ?? Vector3.Zero);
-                // Matrix matWorld = Matrix.Identity;
-                // if (wasUsing && Document.Selection.GetSelectedObjects().Any() && Document.Selection.GetSelectedObjects().FirstOrDefault().MetaData.Has<DataStructures.Geometric.Matrix>("SelectionMatrix")) matWorld = Document.Selection.GetSelectedObjects().FirstOrDefault().MetaData.Get<DataStructures.Geometric.Matrix>("SelectionMatrix").ToXna();
-                // if (wasUsing) matWorld = oldWorld;
+                // matWorld = Matrix.CreateTranslation(Document.Selection.GetSelectionBoundingBox()?.Center.ToXna() ?? Vector3.Zero);
                 float[] world = matWorld.ToCbre().Values.Select(x => (float)x).ToArray();
                 float[] worldDelta = oldWorld.ToCbre().Values.Select(x => (float)x).ToArray();
                 ImGuizmo.SetDrawlist(ImGui.GetWindowDrawList());
                 ImGuizmo.SetRect(vp3d.X, vp3d.Y, vp3d.Width, vp3d.Height);
-                ImGuizmo.Manipulate(ref view[0], ref proj[0], op, MODE.LOCAL, ref world[0], ref worldDelta[0]);
+                bool snap = (Select.SnapStyle == SnapStyle.SnapOnAlt && ViewportManager.Ctrl) || (Select.SnapStyle == SnapStyle.SnapOffAlt && !ViewportManager.Ctrl);
+                float snapAmount = snap && Document.Map.SnapToGrid ? (op.HasFlag(OPERATION.TRANSLATE) ? (float)Document.Map.GridSpacing : 5f) : 0f;
+                ImGuizmo.Manipulate(ref view[0], ref proj[0], op, MODE.LOCAL, ref world[0], ref worldDelta[0], ref snapAmount);
                 if (ImGuizmo.IsUsing()) {
-                    var old = oldWorld;//.Decompose(out var oscl2, out var orot2, out var opos2);
+                    var old = oldWorld;
                     oldWorld = new DataStructures.Geometric.Matrix(worldDelta.Select(x => (decimal)x).ToArray()).ToXna();
-                    // oldWorld.Decompose(out var oscl, out var orot, out var opos);
+                    oldWorld.Decompose(out var oscl, out var orot, out var opos);
                     if (op.HasFlag(OPERATION.TRANSLATE)) {
-                        // oldWorld = Matrix.CreateTranslation(opos);
+                        // matWorld = new DataStructures.Geometric.Matrix(world.Select(x => (decimal)x).ToArray()).ToXna();
+                        // matWorld -= oldWorld;
                     }
                     if (op.HasFlag(OPERATION.ROTATE)) {
                         oldWorld = old * oldWorld;
-                        // oldWorld = Matrix.CreateFromQuaternion(orot2) * Matrix.CreateFromQuaternion(orot);
                     }
                     if (op.HasFlag(OPERATION.SCALE)) {
                         oldWorld = old * oldWorld;
-                        // oldWorld = Matrix.CreateScale(oscl);
                     }
 
                     OnTransforming.Invoke(oldWorld.ToCbre());
@@ -66,11 +67,6 @@ namespace CBRE.Editor.Tools.Widgets {
                     //     obj.MetaData.Set("SelectionMatrix", rot);
                     // }
                 } else if (wasUsing) {
-                    /*new DataStructures.Geometric.Matrix(worldDelta.Select(x => (decimal)x).ToArray()).ToXna().Decompose(out var oscl, out var orot, out var opos);
-                    oldWorld = Matrix.CreateTranslation(opos);
-                    oldWorld *= Matrix.CreateFromQuaternion(orot);
-                    oldWorld += Matrix.CreateScale(oscl);*/
-
                     OnTransformed.Invoke(oldWorld.ToCbre());
                     // foreach (var obj in Document.Selection.GetSelectedObjects()) {
                     //     new DataStructures.Geometric.Matrix(world.Select(x => (decimal)x).ToArray()).ToXna().Decompose(out var scl, out var rot, out var pos);
@@ -80,10 +76,11 @@ namespace CBRE.Editor.Tools.Widgets {
                 else {
                     oldWorld = Matrix.Identity;
                     // matWorld = Matrix.CreateTranslation(Document.Selection.GetSelectionBoundingBox()?.Center.ToXna() ?? Vector3.Zero);
-                    matWorld = Matrix.Identity;
+                    // matWorld = Matrix.Identity;
+                    matWorld = Matrix.CreateTranslation(Document.Selection.GetSelectionBoundingBox()?.Center.ToXna() ?? Vector3.Zero);
                     // if (Document.Selection.GetSelectedObjects().Any() && Document.Selection.GetSelectedObjects().FirstOrDefault().MetaData.Has<DataStructures.Geometric.Matrix>("SelectionMatrix")) matWorld = Document.Selection.GetSelectedObjects().FirstOrDefault().MetaData.Get<DataStructures.Geometric.Matrix>("SelectionMatrix").ToXna();
                     // if (Document.Selection.GetSelectedObjects().Any() && Document.Selection.GetSelectedObjects().FirstOrDefault().MetaData.Has<Quaternion>("SelectionMatrix")) matWorld *= Matrix.CreateFromQuaternion(Document.Selection.GetSelectedObjects().FirstOrDefault().MetaData.Get<Quaternion>("SelectionMatrix"));
-                    matWorld.Translation = Document.Selection.GetSelectionBoundingBox()?.Center.ToXna() ?? matWorld.Translation;
+                    // matWorld.Translation = Document.Selection.GetSelectionBoundingBox()?.Center.ToXna() ?? matWorld.Translation;
                     OnTransformed.Invoke(null);
                 }
                 if (ImGuizmo.IsOver()) {
